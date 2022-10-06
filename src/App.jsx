d86385ed75ab79dad1b01d6665817d764b17ccc3
lmaoclost <renansmoliveira@gmail.com>
2022-10-06 14:32:51 -0300

    Adjusted main route redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 
-import { Outlet } from "react-router-dom";
+import { useEffect } from 'react';
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import {  ContextProvider } from "./Components/utils/global.context";
+import { ContextProvider } from "./Components/utils/global.context";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate("/home");
+    }
+  });
+
   return (
     <ContextProvider>
       <div className="App">
-          <Navbar/>
-          <Outlet/>
-          <Footer/>
+        <Navbar />
+        <Outlet />
+        <Footer />
       </div>
     </ContextProvider>
   );
