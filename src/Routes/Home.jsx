5a218233bffeed29ac065d8ce807e15d83545ebe
lmaoclost <renansmoliveira@gmail.com>
2022-10-06 19:37:48 -0300

    Adjusted main tag

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -5,14 +5,14 @@ import { ContextGlobal } from '../Components/utils/global.context';
 const Home = () => {
   const { data } = useContext(ContextGlobal)
   return (
-    <main>
+    <>
       <h1>Home</h1>
       <div className='card-grid container'>
         {data.length ?
           data.map(dentist => <Card {...dentist} key={dentist.id} />)
           : null}
       </div>
-    </main>
+    </>
   )
 }
 
