76cf0a58d42fc03d7dc97652cb11abbbd949c0f7
lmaoclost <renansmoliveira@gmail.com>
2022-10-06 14:32:57 -0300

    Adjusted navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { ContextGlobal } from './utils/global.context'
+import { ContextGlobal } from './utils/global.context';
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { theme, setDarkTheme, setLightTheme } = useContext(ContextGlobal)
@@ -13,26 +14,26 @@ const Navbar = () => {
 
   return (
     <header className="sticky-top">
-      <nav className="navbar navbar-expand-sm" aria-label="Third navbar example">
+      <nav className={`navbar navbar-expand-sm ${isDarkMode ? "navbar-dark bg-dark" : "navbar-light bg-light"}`} aria-label="Third navbar example">
         <div className="container">
-          <Link to="/">DH Odonto</Link>
+          <Link className={`navbar-brand ${styles.navbarBrand}`} to="/home">DH Odonto</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse justify-content-end" id="navbarsExample03">
             <ul className="navbar-nav mb-2 mb-sm-0">
-              <li className="nav-item">
-                <Link to="/home">Home</Link>
+              <li className={`nav-item ${styles.navBarLink}`}>
+                <Link className='nav-link' to="/home">Home</Link>
               </li>
-              <li className="nav-item">
-                <Link to="/contact">Contact</Link>
+              <li className={`nav-item ${styles.navBarLink}`}>
+                <Link className='nav-link' to="/contact">Contact</Link>
               </li>
-              <li className="nav-item">
-                <Link to="/favs">Favs</Link>
+              <li className={`nav-item ${styles.navBarLink}`}>
+                <Link className='nav-link' to="/favs">Favs</Link>
               </li>
-              <li className="nav-item">
-                <button className="btn btn-primary" onClick={changeTheme} >{isDarkMode ? "☀" : "🌙"} </button>
+              <li className={`nav-item`}>
+                <button className={`btn btn-${isDarkMode ? 'light' : 'dark'} ${styles.btnStyle}`} onClick={changeTheme} >{isDarkMode ? "☀" : "🌙"} </button>
               </li>
             </ul>
           </div>
